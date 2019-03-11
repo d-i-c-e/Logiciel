72543941b1184642fefdd0ae40a964e01c582681
Yannoël <yannoel.martin@hotmail.fr>
2019-03-11 15:08:39 +0100

    avancées

---
+++ Dice/Dice/Views/LoginPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dice.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        private void loginClick(object sender, RoutedEventArgs e)
+        {
+            // test compte BDD
+            (this.Parent as Window).Content = new HomePage();
+        }
+        #endregion
+
+    }
+}
---
+++ Dice/Dice/Views/ParameterPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dice.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour ParameterPage.xaml
+    /// </summary>
+    public partial class ParameterPage : Page
+    {
+        public ParameterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

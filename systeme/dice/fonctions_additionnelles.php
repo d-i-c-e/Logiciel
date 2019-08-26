b0c7c074736573268910116266bbf7a2bdd46f87
Yannoël MARTIN <yannoel.martin@hellomysoft.com>
2019-08-26 09:45:49 +0200

    ajout est_admin

--- systeme/dice/fonctions_additionnelles.php
+++ systeme/dice/fonctions_additionnelles.php
@@ -1,1 +1,7 @@
 <?php
+
+function est_administrateur()
+{
+    $db = new DB();
+    return ($db -> joueur() -> mf_compter() == 0 || get_joueur_courant(MF_JOUEUR_ADMINISTRATEUR) != null && get_joueur_courant(MF_JOUEUR_ADMINISTRATEUR) == 1);
+}
